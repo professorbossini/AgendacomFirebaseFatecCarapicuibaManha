13da9e32f4c597ccf673a3994483a911d1834a70
Fábrica de Software USJT <fabricadesoftwareusjt@gmail.com>
2018-11-08 10:25:21 -0200

    Adicionamos. Falta listar.

--- app/src/main/java/br/com/bossini/agendacomfirebasefateccarapicuibamanha/ListaDeContatosActivity.java
+++ app/src/main/java/br/com/bossini/agendacomfirebasefateccarapicuibamanha/ListaDeContatosActivity.java
@@ -1,5 +1,6 @@
 package br.com.bossini.agendacomfirebasefateccarapicuibamanha;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -34,8 +35,9 @@ public class ListaDeContatosActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(ListaDeContatosActivity.this,
+                                        AdicionaContatoActivity.class);
+                startActivity(intent);
             }
         });
     }
