186ad341717ace15b8f6c12476478571a4b3ad84
Fábrica de Software USJT <fabricadesoftwareusjt@gmail.com>
2018-11-22 10:37:51 -0200

    Pronto.

--- app/src/main/java/br/com/bossini/agendacomfirebasefateccarapicuibamanha/Contato.java
+++ app/src/main/java/br/com/bossini/agendacomfirebasefateccarapicuibamanha/Contato.java
@@ -1,5 +1,7 @@
 package br.com.bossini.agendacomfirebasefateccarapicuibamanha;
 
+import com.google.firebase.database.Exclude;
+
 public class Contato {
     private String id;
     private String nome, fone, email;
@@ -19,6 +21,7 @@ public class Contato {
         setEmail(email);
     }
 
+    @Exclude
     public String getId() {
         return id;
     }
@@ -49,5 +52,15 @@ public class Contato {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Contato{" +
+                "id='" + id + '\'' +
+                ", nome='" + nome + '\'' +
+                ", fone='" + fone + '\'' +
+                ", email='" + email + '\'' +
+                '}';
     }
 }
--- app/src/main/java/br/com/bossini/agendacomfirebasefateccarapicuibamanha/ListaDeContatosActivity.java
+++ app/src/main/java/br/com/bossini/agendacomfirebasefateccarapicuibamanha/ListaDeContatosActivity.java
@@ -1,21 +1,39 @@
 package br.com.bossini.agendacomfirebasefateccarapicuibamanha;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ListaDeContatosActivity extends AppCompatActivity {
+
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference contatosReference;
+
+    private void configuraFirebase (){
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        contatosReference = firebaseDatabase.getReference("contatos");
+    }
 
     private ListView contatosListView;
     private ArrayAdapter <Contato> contatosAdapter;
@@ -40,27 +58,99 @@ public class ListaDeContatosActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        configuraFirebase();
+        configuraLongClickListener();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_lista_de_contatos, menu);
-        return true;
+    private void configuraLongClickListener(){
+        contatosListView.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                AlertDialog.Builder apagarAtualizarDialogoBuilder =
+                        new AlertDialog.Builder(ListaDeContatosActivity.this);
+                apagarAtualizarDialogoBuilder.
+                        setNegativeButton(R.string.deletar_contato, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //apagar o cara que sofreu o clique longo
+                                Contato contato = contatos.get(position);
+                                String id = contato.getId();
+                                contatosReference.child(id).removeValue();
+                                Toast.makeText(ListaDeContatosActivity.this,
+                                        getString(R.string.contato_removido),
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }).setPositiveButton(R.string.atualizar_contato,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //atualizar o cara que sofreu o clique longo
+                                final Contato contato = contatos.get(position);
+                                AlertDialog.Builder atualizarBuilder =
+                                        new AlertDialog.Builder(ListaDeContatosActivity.this);
+                                LayoutInflater inflater = LayoutInflater.from(ListaDeContatosActivity.this);
+                                View view = inflater.inflate(R.layout.activity_adiciona_contato, null);
+                                atualizarBuilder.setView(view);
+                                final AlertDialog atualizarDialogo = atualizarBuilder.create();
+                                final EditText nomeEditText = view.findViewById(R.id.nomeEditText);
+                                nomeEditText.setText(contato.getNome());
+                                final EditText foneEditText = view.findViewById(R.id.foneEditText);
+                                foneEditText.setText(contato.getFone());
+                                final EditText emailEditText = view.findViewById(R.id.emailEditText);
+                                emailEditText.setText(contato.getEmail());
+                                FloatingActionButton floatingActionButton =
+                                        view.findViewById(R.id.fab);
+
+                                floatingActionButton.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        String nome =
+                                                nomeEditText.getEditableText().toString();
+                                        String fone =
+                                                foneEditText.getEditableText().toString();
+                                        String email =
+                                                emailEditText.getEditableText().toString();
+                                        String id = contato.getId();
+                                        Contato atualizarContato = new Contato(id, nome, fone, email);
+                                        contatosReference.child(id).setValue(atualizarContato);
+                                        Toast.makeText(ListaDeContatosActivity.this,
+                                                getString(R.string.atualizar_contato),
+                                                Toast.LENGTH_SHORT).show();
+                                        atualizarDialogo.cancel();
+
+                                    }
+                                });
+
+                                atualizarDialogo.show();
+                            }
+                        });
+                apagarAtualizarDialogoBuilder.create().show();
+                return false;
+            }
+        });
     }
-
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected void onStart() {
+        super.onStart();
+        contatosReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                contatos.clear();
+                for (DataSnapshot filho : dataSnapshot.getChildren()){
+                    Contato contato = filho.getValue(Contato.class);
+                    contato.setId(filho.getKey());
+                    contatos.add(contato);
+                }
+                contatosAdapter.notifyDataSetChanged();
+            }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(ListaDeContatosActivity.this,
+                        getString(R.string.erro_firebase),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
